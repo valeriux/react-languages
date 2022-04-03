e17398f269524a80de9822f897e3e1fb9d81f43d
Valeri <valeriliux@icloud.com>
2022-04-03 23:41:23 +0100

    Adding bootstrap and fixing react.18

--- src/index.js
+++ src/index.js
@@ -1,47 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import reportWebVitals from './reportWebVitals';
-import HttpApi from 'i18next-http-backend';
-import i18next from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HttpApi from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 i18next
-.use(i18next)
-.use(LanguageDetector)
-.use(HttpApi)
-.init({
-  fallbackLng: 'en',
-  debug: true,
-  detection: {
-  // Order and from where user language should be detected
-  order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
-  caches: ["cookie"],
-  },
-  backend: {
-    loadPath
-  }
-});
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en', 'ar', 'fr'],
+    fallbackLng: 'en',
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+      caches: ['cookie'],
+    },
+    // react: { useSuspense: false },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  })
 
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-function App() {
-  const { t } = useTranslation();
-
-  return <h2>{t('Welcome to React')}</h2>;
-}
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+)
