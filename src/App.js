05f586a72d2b997c366fa1442f27a92efb59c2e8
Valeri <valeriliux@icloud.com>
2022-04-04 02:22:37 +0100

    Fixing problem with on select language

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React, { useEffect } from "react"
 import { useTranslation } from "react-i18next"
 import i18n from 'i18next'
-import { useEffect } from "react"
 import cookies from "js-cookie"
+import classNames from 'classnames'
+
 
 const languages = [
   {
@@ -19,7 +21,6 @@ const languages = [
     name: 'العربية',
     dir: 'rtl',
     country_code: 'sa',
-    dir: "rtl",
   },
 ]
 
@@ -30,9 +31,10 @@ const GlobeIcon = (width = 24, height = 24) => (
 )
 
 function App() {
-  const currentLanguageCode = cookies.get('i18n') || 'en'
-  const currentLanguage = languages.find(l => l.code === currentLanguageCode)
+  const currentLanguageCode = cookies.get('i18next') || 'en'
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
   const { t } = useTranslation()
+
   const releaseDate = new Date('2021-03-07')
   const timeDifference = new Date() - releaseDate;
   const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
@@ -44,41 +46,49 @@ function App() {
 
   return (
     <div className="container">
-      <div className="d-flex justify-content-end">
-        <div className="dropdown">
-          <button className="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-            <GlobeIcon />
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li>
-              <span className="dropdown-item-text">{t('language')}
-
-              </span>
-            </li>
-            {languages.map(({ code, name, country_code }) => (
-              <li key={country_code}>
-                <button className="dropdown-item"
-                  onClick={() => i18n.changeLanguage(code)}
-                  disabled={code === currentLanguageCode}
-                >
-                  <span
-                    className={`flag-icon flag-icon-${country_code} mx-2`}
-                    style={{ opacity: code === currentLanguageCode ? 0.5 : 1 }}
-                  ></span>
-                  {name}
-                </button>
+      <div className="language-select">
+        <div className="d-flex justify-content-end align-items-center language-select-root">
+          <div className="dropdown">
+            <button
+              className="btn btn-link dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <GlobeIcon />
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <span className="dropdown-item-text">{t('language')}</span>
               </li>
-            ))}
-          </ul>
+              {languages.map(({ code, name, country_code }) => (
+                <li key={country_code}>
+                  <button
+                    className={classNames('dropdown-item', {
+                      disabled: currentLanguageCode === code,
+                    })}
+                    onClick={() => {
+                      i18n.changeLanguage(code)
+                    }}
+                  >
+                    <span
+                      className={`flag-icon flag-icon-${country_code} mx-2`}
+                      style={{
+                        opacity: currentLanguageCode === code ? 0.5 : 1,
+                      }}
+                    ></span>
+                    {name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-
-
       <div className="d-flex flex-column align-items-start">
-        <h1 className="font-weight-normal mb-3">
-          {t('welcome_message')}
-          <p>{t('days_since_release', { number_of_days })}</p>
-        </h1>
+        <h1 className="font-weight-normal mb-3">{t('welcome_message')}</h1>
+        <p>{t('days_since_release', { number_of_days })}</p>
       </div>
     </div>
   );
