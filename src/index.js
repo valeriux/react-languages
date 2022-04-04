4ed7c70d6020de834e3abe54e22fe0fc21743314
Valeri <valeriliux@icloud.com>
2022-04-04 01:48:03 +0100

    Adding title for multilanguage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createRoot } from 'react-dom/client';
-
-import i18next from 'i18next'
+import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'flag-icon-css/css/flag-icon.min.css'
 
-
-i18next
+i18n
   .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
@@ -22,21 +20,38 @@ i18next
     debug: false,
     // Options for language detector
     detection: {
-      order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+      order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
       caches: ['cookie'],
     },
-    // react: { useSuspense: false },
+    //react: { useSuspense: false },
     backend: {
-      loadPath: '/assets/locales/{{lng}}/translation.json',
+      loadPath: '/assets/translations/{{lng}}/translation.json',
     },
   })
 
+const loadingMarkup = (
+  <div className='py-4 text-center'>
+    <h2>Loading...</h2>
+  </div>
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+)
 
-root.render(
+ReactDOM.render(
+  <Suspense fallback={loadingMarkup}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+  </Suspense>,
+  document.getElementById('root')
+);
+
+
+//import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
